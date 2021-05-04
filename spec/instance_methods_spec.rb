0fc4ea62abe586079b9b31f984d0b23054a986d6
Liz Burton <liz.burton147@gmail.com>
2021-05-04 14:41:25 -0400

    consolidate tests with previous lab

--- spec/instance_methods_spec.rb
+++ spec/instance_methods_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 describe 'Dog' do
   it 'is defined' do
     expect(defined?(Dog)).to be_truthy
@@ -29,6 +31,23 @@ describe 'Dog' do
 
       fido = Dog.new
       fido.sit
+    end
+  end
+
+  context 'variables defined in lib/dog.rb' do
+    it 'defines a local variable fido as an instance of a Dog' do
+      fido = get_variable_from_file("lib/dog.rb", "fido")
+      expect(fido).to be_a(Dog)
+    end
+
+    it 'defines a local variable snoopy as an instance of a Dog' do
+      snoopy = get_variable_from_file("lib/dog.rb", "snoopy")
+      expect(snoopy).to be_a(Dog)
+    end
+
+    it 'defines a local variable lassie as an instance of a Dog' do
+      lassie = get_variable_from_file("lib/dog.rb", "lassie")
+      expect(lassie).to be_a(Dog)
     end
   end
 end
@@ -64,6 +83,18 @@ describe 'Person' do
 
       ada = Person.new
       ada.walk
+    end
+  end
+
+  context 'variables defined in lib/person.rb' do
+    it 'defines a local variable adele_goldberg as an instance of a Person' do
+      adele_goldberg = get_variable_from_file("lib/person.rb", "adele_goldberg")
+      expect(adele_goldberg).to be_a(Person)
+    end
+
+    it 'defines a local variable alan_kay as an instance of a Person' do
+      alan_kay = get_variable_from_file("lib/person.rb", "alan_kay")
+      expect(alan_kay).to be_a(Person)
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,24 @@ require_relative '../lib/person'
 RSpec.configure do |config|
   config.order = :default
 end
+
+def file_includes_class_definition(file,class_name)
+   search_result = File.open(file).grep(/class #{Regexp.quote(class_name)}/).first
+   if search_result
+     search_result.include?("class #{class_name}")
+   else
+     message = "#{file} does not appear to include a class definition for #{class_name}"
+     raise NameError, message
+   end
+end
+
+def get_variable_from_file(file, variable)
+  file_scope = binding
+  file_scope.eval(File.read(file))
+
+  begin
+    return file_scope.local_variable_get(variable)
+  rescue NameError
+    raise NameError, "local variable `#{variable}' not defined in #{file}."
+  end
+end
