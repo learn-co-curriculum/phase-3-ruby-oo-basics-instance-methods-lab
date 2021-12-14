094bebdaee2532228ca5943b48c74f8815acbdf6
Brooks_Wilcox <brookswill88@gmail.com>
2021-12-13 18:13:29 -0700

    completed assignment

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+    def bark
+        puts "Woof!"
+    def sit 
+            puts "The Dog is sitting"
+    end
+    end
+end
+
+fido = Dog.new
+fido.bark
+# fido.sit
+
+
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+class Person 
+
+    def talk
+        puts "Hello World!"
+    def walk
+        puts "The Person is walking"
+    end
+    end
+end        
