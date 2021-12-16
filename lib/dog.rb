53bebc2e9d57ecc1580e366b5ceae44dfc9a5f4a
Robert Gaspar <gaspar.robert7@gmail.com>
2021-12-16 10:27:14 -0600

    commit tree 436ba9a1359f67e2342a5badac26e8a17fdb0488

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+class Dog
+    def bark
+        puts "Woof!"
+    end
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+fido = Dog.new
+fido.bark
+
+snoopy = Dog.new
+snoopy.bark
