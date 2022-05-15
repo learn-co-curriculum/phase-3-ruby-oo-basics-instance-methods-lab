13fd4d631d8c4bb5814d3d4af1fa4ae5c28193c2
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-05-15 14:31:42 -0400

    commit tree 754395fad44f8f1c5f6b08c47913665c260bf470

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def bark
+    puts "Woof!"
+  end
+  def sit 
+    puts "The Dog is sitting"
+  end
+end
+
+fido = Dog.new
+snoopy = Dog.new
+
+fido.bark 
+snoopy.bark
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+class Person
+  def talk 
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+end 
