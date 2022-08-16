2092f23248012b7e3fdce641796d3396a63d25ee
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-08-16 16:47:58 +0300

    commit tree 2577dcadc6e64db171487b4e7f029fb3de2dcc2e

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    def bark 
+        puts 'Woof!'
+    end
+end
+class Dog
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+fido =Dog.new
+fido.bark
+fido.sit 
+
+snoopy = Dog.new
+snoopy.bark
+snoopy.sit 
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+    def talk 
+        puts 'Hello World!'
+    end
+end
+
+class Person
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+yvonne = Person.new
+yvonne.talk
+yvonne.walk
+
+martha =Person.new
+martha.talk
+martha.walk
