5a74c8434a60d6a6bb3a0d53fcead703094ca40d
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-08-18 11:16:13 +0300

    commit tree d90bb3a9e4a4fe5601a0166a20b0b4c16f8664a6

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def talk 
+        puts "Hello World!"
+    end
+    def walk 
+        puts "The Person is walking"
+    end
+end
+
+person1 = Person.new
+person1.talk
+person1.walk
+
+
