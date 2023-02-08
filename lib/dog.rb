ea5b483d6735f5e79cd3b90b1b000111b490b3bc
max101g <maxgithinji101@gmail.com>
2023-02-09 00:30:44 +0300

    Ruby commit

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+    def bark
+        puts "Woof!"
+    end
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+fido = Dog.new
+
+class Person
+    def talk
+        puts "Hello World!"
+    end
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+ada = Person.new
