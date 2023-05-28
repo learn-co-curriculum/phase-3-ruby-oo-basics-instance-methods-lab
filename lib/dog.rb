2edc9860e3f78c6e0ca8833646123091f3f1acd6
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-05-27 20:10:42 -0700

    add Instance methods

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    #class body
+
+
+    #instance method dfinition
+    def bark
+        puts "woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+fido = Dog.new
+fido.bark
+
+snoopy = Dog.new
+snoopy.bark
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+
+    def talk
+        puts "Hello world!"
+    end
+
+    def walk
+        puts "The person is walking"
+    end
+
+end
+emmanuel = Person.new
+emmanuel.talk
+
+kipsang = Person.new
+kipsang.walk
